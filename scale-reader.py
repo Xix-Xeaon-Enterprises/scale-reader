a98fbdfe77ea9083934f4dbb854569d82101dc82
xix xeaon <xix.xeaon@gmail.com>
2018-01-06 08:33:57 +0100

    Info now in readme

--- scale-reader.py
+++ scale-reader.py
@@ -1,31 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
-
-"""
-LINUX
-
-! usb.core.USBError: [Errno 13] Access denied (insufficient permissions)
-
-$ echo /etc/udev/rules.d/99-scale-reader.rules
-SUBSYSTEM=="usb", ATTR{idVendor}=="0922", ATTR{idProduct}=="8005", GROUP="dialout", MODE="0666"
-$ sudo udevadm control --reload-rules
-
-
-
-WINDOWS
-python3
-pygi aio bundle
-pip install pyusb
-libusb-win32
-
-Using the libusb-win32 Filter Wizard, install a device filter for the scale. It will show up in the list as “vid:0922 pid:8005 rev:0100 USB Input Device”.
-
-
-"""
-
-
 # stdlib
 import array, threading, random, time
 
